6ea169229ffb415edbd2e67b0034eccc8b8e0b1a
Mihalj97 <mihalj.tandi5@gmail.com>
2022-01-26 17:01:13 +0100

    used operator

--- src/App.js
+++ src/App.js
@@ -7,20 +7,30 @@ function App() {
         { id: 3, text: 'Comment three' }
     ]
 
+    const loading = false
+    const showComments = true
+
+    if(loading) return <h1>Loading...</h1>
+
+    const commentBlock =(
+        <div className="comments">
+        <h3>Comments ({comment.length})</h3>
+        <ul>
+            {comment.map((comment, index) => (
+                <li key={index}>{comment.text}</li>
+            ))}
+        </ul>
+
+        </div>)
+
     return (
         <div className='container'>
             <h1>{title.toUpperCase()}</h1>
             <p>{body}</p>
 
-            <div className="comments">
-                <h3>Comments ({comment.length})</h3>
-                <ul>
-                    {comment.map((comment, index) => (
-                        <li key={index}>{comment.text}</li>
-                    ))}
-                </ul>
+            {showComments && commentBlock}
+            
 
-            </div>
 
         </div>
     )
