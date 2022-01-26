d9251751e6217c4fe3c5deac4cd0e386478e4c0d
Mihalj97 <mihalj.tandi5@gmail.com>
2022-01-26 17:39:41 +0100

    used props

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
+import Header from "./components/Header"
+
 function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comment = [
-        { id: 1, text: 'Comment one' },
-        { id: 2, text: 'Comment two' },
-        { id: 3, text: 'Comment three' }
-    ]
-
-    const loading = false
-    const showComments = true
-
-    if(loading) return <h1>Loading...</h1>
-
-    const commentBlock =(
-        <div className="comments">
-        <h3>Comments ({comment.length})</h3>
-        <ul>
-            {comment.map((comment, index) => (
-                <li key={index}>{comment.text}</li>
-            ))}
-        </ul>
-
-        </div>)
-
     return (
+        <>
+        <Header text="Hello World" />
         <div className='container'>
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-
-            {showComments && commentBlock}
+            <h1>My App</h1>
             
-
-
         </div>
+        </>
     )
 }
 
