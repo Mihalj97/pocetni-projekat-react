7f6312edbf642fa8f2ccdbd7f5465461b5097fc9
Mihalj97 <mihalj.tandi5@gmail.com>
2022-01-26 18:30:27 +0100

    used headerStyles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
-function Header(props) {
-    return(
-    <header>
+import PropTypes from 'prop-types';
+
+
+function Header({ text }) {
+    const headerStyles = {
+        backgroundColor: 'blue',
+        color: 'red'
+    }
+
+    return (
+        <header style={}>
         <div className="container">
-            <h2>{props.text}</h2>
+            <h2>{text}</h2>
         </div>
     </header>
     )
+}
+
+Header.defaultProps = {
+    text: 'Feedback UI',
+}
+
+Header.propTypes = {
+    text: PropTypes.string,
 }
 
 export default Header
