2e8de328ad3e849ea97572b8e53e05dea915f475
Mihalj97 <mihalj.tandi5@gmail.com>
2022-01-27 17:21:15 +0100

    Use params

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
-import { BrowserRouter as Router, Route, Routes, NavLink } from "react-router-dom";
-import Card from "./components/shared/Card";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
@@ -9,6 +8,7 @@ import FeedbackFrom from "./components/FeedbackFrom";
 import { v4 as uuidv4 } from 'uuid'
 import AboutPage from "./pages/AboutPage";
 import AboutIconLink from "./components/AboutIconLink";
+import Post from "./components/Post";
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -40,18 +40,8 @@ function App() {
                             </>
                         }></Route>
                     <Route path='/about' element={<AboutPage />} />
+                    <Route path='/post/:id/:name' element={<Post />} />
                 </Routes>
-
-                <Card>
-
-                    <NavLink to='/' activeClassName='active'>
-                        Home
-                    </NavLink>
-
-                    <NavLink to='/about' activeClassName='active' >
-                        About
-                    </NavLink>
-                </Card>
 
                 <AboutIconLink />
             </div>
