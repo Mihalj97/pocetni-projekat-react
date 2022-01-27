33a254b073303d79ebe7612102022a135de482c7
Mihalj97 <mihalj.tandi5@gmail.com>
2022-01-27 14:07:25 +0100

    Real-Time validation

--- src/components/FeedbackFrom.jsx
+++ src/components/FeedbackFrom.jsx
@@ -4,9 +4,21 @@ import Button from "./shared/Button";
 
 function FeedbackFrom() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
-        setText(e.target.value);
+        if (text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if (text !== '' && text.trim().length <= 10) {
+            setMessage('Text must be at least 10 characters')
+            setBtnDisabled(true)
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+        setText(e.target.value)
     }
 
     return <Card>
@@ -16,8 +28,9 @@ function FeedbackFrom() {
             <div className="input-group">
                 <input onChange={handleTextChange} type="text"
                 placeholder='Write a review' value={text} />
-                <Button type="submit">Send</Button>
+                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
             </div>
+            {message && <div className="message">{ message }</div>}
         </form>
     </Card>;
 }
