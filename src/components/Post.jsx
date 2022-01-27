63d3f940796361d80967b98810633487ffe51303
Mihalj97 <mihalj.tandi5@gmail.com>
2022-01-27 17:42:43 +0100

    Navigate and nested routes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,27 @@
-import {useParams} from 'react-router-dom'
+import React from 'react';
+import { Navigate, useNavigate, Routes, Route } from 'react-router-dom';
 
 function Post() {
-    const params = useParams()
+    const status = 200;
+    const navigate = useNavigate()
 
-    return <div>
-        <h1>Post {params.id}</h1>
-        <h1>Name {params.name}</h1>
-  </div>;
+    const onClick = () => {
+        console.log('Hello');
+        navigate('/about');
+    }
+
+    if (status === 400) {
+        <Navigate to='/notfound' />
+    }
+    return (
+        <div>
+            <h1>Post</h1>
+            <button onClick={onClick}>Click</button>
+            <Routes>
+                <Route path='/show' element={<h1>Hello world</h1>} />
+            </Routes>
+        </div>
+    )
 }
 
 export default Post;
